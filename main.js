5b9160e24c583fe0f47f7dc6740899aaac722809
Natalia Reggiani <nreggiani77@gmail.com>
2022-07-20 12:08:26 -0300

    complete navbar

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 let menu = document.querySelector('.menu') 
 let ul = document.querySelector('#lista-menu') 
-
-
+let navPage = document.querySelector('.nav-pages')
 
 menu.addEventListener('click', function(){
-    ul.classList.toggle('none')
+    ul.classList.toggle('none');
+    navPage.classList.toggle('nav-pages_black');
+})
+
+
+
+
+
+let items = document.querySelectorAll('.listas')
+console.log(items)
+
+
+items.forEach(el=>{
+    el.addEventListener('click', function(e){
+        e.preventDefault();
+        items.forEach(el => el.classList.remove('color'))
+        this.classList.add('color')
+    })
 })
